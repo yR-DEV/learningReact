10447be8607cb0486f10bab0195a7bf30c83681e
Zack Claar <claar.zack@gmail.com>
2015-10-26 10:23:22 -0600

    restarting react tutorial, going to go through it and then think of something I could build to learn it more thoroughly

--- reactPractice/js/helloworld.jsx
+++
@@ -1,8 +0,0 @@
-React.render(
-  <h1>Hello, World!</h1>
-  document.getElementById('hello')
-);
-
-
-//You have to run the react stuff in an http-server, should be installed,
-//in order to properly run the jsc trandformer you have to use the http-server
